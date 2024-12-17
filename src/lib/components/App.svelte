c9240d80b91a62f1a99287e894408d1ff5b4eb74
Nurman2021 <nurman.awaluddin@student.unismuh.ac.id>
2024-12-17 11:59:19 -0800

    first threlte

---
+++ src/lib/components/App.svelte
@@ -0,0 +1,45 @@
+<script>
+  import { Canvas } from '@threlte/core'
+  import { Environment } from '@threlte/extras'
+  import Scene from './Scene.svelte'
+  import {moveCamera} from './Scene.svelte'
+  import { onMount } from 'svelte';
+  import gsap from 'gsap';
+
+  onMount(()=>{
+  	const ctx = gsap.context((self)=>{
+  		gsap.to(moveCamera,{
+  			x:20, y:5, z:0,
+  		})
+  	})
+  		return () => ctx.revert(); // <- Cleanup!
+  })
+</script>
+
+<Canvas>
+  <Scene moveCamera={{x:10, y:20, z:8}} rotateCamera={{x:0,y:1,z:0}} />
+</Canvas>
+
+<section>
+	<h1>this is sparta</h1>
+	
+</section>
+
+<style>
+	section{
+		position: absolute;
+		top: 0;
+		width: 100vw;
+		height: 100vh;
+		z-index: 100;
+		/*background-color: red; */
+	}
+
+	h1{
+		color: white;
+	}
+
+	Canvas{
+
+	}
+</style>
---
+++ src/lib/components/Scene.svelte
@@ -0,0 +1,53 @@
+<script>
+  import { T, useFrame, useThrelte } from '@threlte/core'
+  import Mecha from './models/Mecha.svelte'
+  import { OrbitControls} from '@threlte/extras'
+
+  export let moveCamera = {x,y,z};
+  export let rotateCamera = {x,y,z};
+
+</script>
+
+<!-- <T.Group rotation.y={rotation}> -->
+<T.Group>
+  <T.PerspectiveCamera
+    makeDefault
+    position={[moveCamera.x, moveCamera.y, moveCamera.z]}
+    fov={15}
+    on:create={({ ref }) => {
+      ref.lookAt(rotateCamera.x, rotateCamera.y, rotateCamera.z)
+    }}
+  >
+      <OrbitControls
+
+  />
+  </T.PerspectiveCamera>
+</T.Group>
+
+  <Mecha/>
+
+<!-- Floor -->
+<T.Mesh  rotation.x={-Math.PI / 2} receiveShadow position.y={0.05}>
+  <T.CircleGeometry args={[2, 40]} />
+  <T.MeshStandardMaterial
+    color="#cc4f55"
+  />
+</T.Mesh>
+
+<T.Mesh  rotation.x={-Math.PI / 2} receiveShadow>
+  <T.CircleGeometry args={[8, 40]} />
+  <T.MeshStandardMaterial
+    color="#252535"
+  />
+</T.Mesh>
+
+<T.DirectionalLight
+  position={[6, 6, 6]}
+  intensity={1.7}
+  castShadow
+/>
+<T.AmbientLight color="#ffffff" intensity={0.3}/>
+
+<T.PointLight color="#ffffff" position={[0, 10, 10]}/>
+
+
